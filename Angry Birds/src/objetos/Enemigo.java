15e2df632be0bed52f7a6cfdf4011d364556ad2d
Pablo del Campo Olano <pablodel.campo@opendesuto.es>
2022-11-09 11:33:25 +0100

    Añadido el metodo contienePunto y eliminado el constructor vacio

--- Angry Birds/src/objetos/Enemigo.java
+++ Angry Birds/src/objetos/Enemigo.java
@@ -1,15 +1,24 @@
 package objetos;
 
+import java.awt.Point;
+
 public class Enemigo extends ObjetoPrimitivo{
 	protected int radio;
-	public Enemigo() {
-		// TODO Auto-generated constructor stub
-		super();
-		radio=2;
-	}
 	public Enemigo(int x,int y,int radio) {
 		super(x,y);
 		this.radio=radio;
+	}
+	public void setX(int x) {
+		if (x-radio<0) {
+		}else {
+			this.x=x;
+		}
+	}
+	public void setY(int y) {	
+		if (y-radio<0) {
+		}else {
+			this.y=y;
+		}
 	}
 	public int getRadio() {
 		return radio;
@@ -17,5 +26,8 @@ public class Enemigo extends ObjetoPrimitivo{
 	public void setRadio(int radio) {
 		this.radio = radio;
 	}
-
+	public boolean contienePunto(Point punto) {
+		double dist= Math.sqrt(Math.pow(x-punto.getX(),2)+Math.pow(y-punto.getY(),2));
+		return dist<= radio;
+	}
 }
