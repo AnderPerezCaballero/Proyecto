66b05bc75a985cc9493a82fedbed38e8ccc6be44
Diego Merino Márquez <diego.merino@opendeusto.es>
2022-12-02 17:19:30 +0100

    Problemas de compatibilidad resueltos

--- Angry Birds/src/gui/inicio/VentanaInicioPlay2.java
+++ Angry Birds/src/gui/inicio/VentanaInicioPlay2.java
@@ -10,17 +10,18 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import gui.componentes.BlinkLabel;
 import gui.juego.VentanaNiveles;
 
 @SuppressWarnings("serial")
 public class VentanaInicioPlay2 extends VentanaInicio{
-	private KeyListener kl;
-
+private KeyListener kl;
+	
 	public VentanaInicioPlay2() {
 		super();
-
+		
 		panelAbajo = new JPanel();
 		panelAbajo.setOpaque(false);
 		panelPrincipal.add(panelAbajo, BorderLayout.SOUTH);
@@ -29,35 +30,40 @@ public class VentanaInicioPlay2 extends VentanaInicio{
 		BlinkLabel bl = new BlinkLabel("PULSA CUALQUIER TECLA PARA JUGAR");
 		panelAbajo.add(bl);
 		bl.setFont(new Font("Arial", Font.CENTER_BASELINE,15));
-
+		
 		panelPrincipal.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				fin();
+				VentanaNiveles nv = new VentanaNiveles();
+				dispose();
+				
 			}
 		});
-
+		
 		kl = new KeyAdapter() {
-
+			
 			@Override
 			public void keyReleased(KeyEvent e) {
 				if (e.getKeyCode() != KeyEvent.VK_ESCAPE) {
-					fin();
+					VentanaNiveles nv = new VentanaNiveles();
+					dispose();				
 				} 
+						
 			}
-		};
 
+		};
+		
 		this.addKeyListener(kl);
 		this.addKeyListener(escCerrar);
+		
 		this.setVisible(true);
 	}
 	
-	/** Cierra la ventana y da inicio al juego
-	 * 
-	 */
-	private void fin() {
-		setEstaCerrada(true);
-		dispose();
-		new VentanaNiveles().setVisible(true);
-	}
+	public static void main(String args[]) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new VentanaInicioPlay2();
+			}
+		});
+	}		
 }
