1d530ddc5e1679dd5cd4e606321dddeaa1ad0ca7
Diego Merino Márquez <diego.merino@opendeusto.es>
2022-12-03 23:54:07 +0100

    Resuelto error en clase pajaroAmarillo
    El constructor estaba mal implementado

--- Angry Birds/src/objetos/pajaros/PajaroAmarillo.java
+++ Angry Birds/src/objetos/pajaros/PajaroAmarillo.java
@@ -1,10 +1,13 @@
 package objetos.pajaros;
 
+import java.awt.Color;
+
 public class PajaroAmarillo extends Pajaro {
 	protected Habilidad habilidad;
-	public PajaroAmarillo(int x,int y, int vX, int vY,int radio) {
-		super(x,y,radio);
-		habilidad = Habilidad.DESTELLO;
-		// TODO Auto-generated constructor stub
+
+	public PajaroAmarillo(int x, int y, int radio, Color color, Habilidad habilidad) {
+		super(x, y, radio, color);
+		this.habilidad = habilidad;
 	}
+	
 }
