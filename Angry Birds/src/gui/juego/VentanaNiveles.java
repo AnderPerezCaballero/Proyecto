6a272b391a71bf0438d2e6b932a7fb3149f97f0a
Diego Merino Márquez <diego.merino@opendeusto.es>
2023-01-16 01:13:21 +0100

    Implementada la selección de niveles a través del teclado

--- Angry Birds/src/gui/juego/VentanaNiveles.java
+++ Angry Birds/src/gui/juego/VentanaNiveles.java
@@ -9,7 +9,6 @@ import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
